d8a8f921200f1b6adf0109bd60fd0cc3e9747235
Ilya Vorobyev <ilvavorobyev@gmail.com>
2020-04-03 17:07:37 +0500

    initial commit

---
+++ MyPlaces/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  MyPlaces
+//
+//  Created by Ilya Vorobyev on 03/04/2020.
+//  Copyright © 2020 Ilya Vorobyev. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+    let restaurantNames =  ["Рататуй", "Своя компания", "Burger King", "KFC", "PLOV", "Рыболов"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        
+        return cell
+    }
+
+}
